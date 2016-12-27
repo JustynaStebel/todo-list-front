b7734fa10810b0910032d504c1bdeff41f18168c
JustynaStebel <justyna.stebel@gmail.com>
2016-12-27 20:19:24 +0100

    Add adapter

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  value: "",
-  actions: {
-    onEnter(name) {
-      if (name) {
-        this.get('addTask')({ name });
-      }
-      this.set('value', "");
-    }
-  }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,21 +1,4 @@
 import Ember from 'ember';
-import db from '../utils/db';
 
 export default Ember.Route.extend({
-  model() {
-    return db.tasks();
-  },
-
-  actions: {
-
-    addTask(task) {
-      db.addTask(task);
-      this.refresh();
-    },
-    removeTask(task) {
-      db.removeTask(task);
-      this.refresh();
-    }
-
-  }
 });
